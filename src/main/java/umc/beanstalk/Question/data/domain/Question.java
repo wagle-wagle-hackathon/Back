deac7a5b2cd7b8db24a2b637df593840ca6d9cdc
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-02-16 22:42:30 +0900

    Feat: Choice, Question domain

---
+++ src/main/java/umc/beanstalk/Question/data/domain/Question.java
@@ -0,0 +1,25 @@
+package umc.beanstalk.Question.data.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Question {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long questionId;
+
+	private String qText;
+}
